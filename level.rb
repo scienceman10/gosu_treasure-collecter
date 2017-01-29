7319d3dfc04e4f6d2e68b0f5105fa25fa5884c21
Benjamin <dogle22@outlook.com>
2017-01-28 18:08:44 -0700

    some methods

--- level.rb
+++ level.rb
@@ -19,12 +19,39 @@ class Level
     end
   end
   def setup_level
+    @level_data.each_with_index do |row_data, row|
+      column = 0
+      row_data.each_char do |cell_type|
+        tile = Tile.new(@window, column, row, cell_type)
+        if tile.is_start?
+          @player.move_to(column, row)
+        end
+        @tiles.push(tile)
+        column += 1
+      end
+    end
   end
   def button_down(id)
-    if id == 'h'
-      @window.hide
-      puts ""
+    # if id == Gosu::KbH
+      # @window.hide
+      # puts ""
+    # end
+    if level_over?
+      return
     end
+    column_change = 0
+    row_change = 0
+    if id == Gosu::KbLeft
+      column_change = -1
+    elsif id == Gosu::KbRight
+      column_change = 1
+    elsif id == Gosu::KbUp
+      row_change = -1
+    elsif id == Gosu::KbDown
+      row_change = 1
+    end
+  end
+  def move_valid?(@player, column_change, row_change)
   end
   def update
   end
